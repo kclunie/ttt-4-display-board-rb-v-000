bf8e38e2cee864b9aabe01f45d1a06f3b57de343
kcluniesa <kcluniesa@yahoo.com>
2018-09-16 01:22:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -19,12 +19,20 @@ def display_board(board)
   puts "   |   |   "
 end
 
-#board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-#def display_board(board)
- # puts "  #{board[0]} |   |   "
-  #puts "-----------"
-  #puts "   |   |   "
-  #puts "-----------"
-  #puts "   |   |   "
-#end
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
 
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end

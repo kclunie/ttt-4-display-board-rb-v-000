a94400ab42039b8bdd72c601d91f39853e840f2e
kcluniesa <kcluniesa@yahoo.com>
2018-09-16 01:30:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -36,3 +36,12 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts "  #{board[0]} | #{board[1]} | #{board{[2]} "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
